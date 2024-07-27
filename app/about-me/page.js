2abb0bfb66b80cb595eaa84fbd34e48a281b55d9
Natasha Osborne <admin@natashaosborne.com>
2024-07-27 03:08:19 -0600

    add routing for work

---
+++ app/about-me/page.js
@@ -0,0 +1,9 @@
+export default function Work() {
+  return (
+    <main>
+      <div className="container">
+        <p style={{ textAlign: "center" }}>About Me</p>
+      </div>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Main() {
   return (
     <main>
       <div className="container place-items-center">
