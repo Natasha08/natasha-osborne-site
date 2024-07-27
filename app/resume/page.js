3573fc50eeff61d5a17d8372a5949d991546afd2
Natasha Osborne <admin@natashaosborne.com>
2024-07-27 03:08:43 -0600

    add routing for resume

---
+++ app/resume/page.js
@@ -0,0 +1,9 @@
+export default function Work() {
+  return (
+    <main>
+      <div className="container">
+        <p style={{ textAlign: "center" }}>Resume</p>
+      </div>
+    </main>
+  );
+}
