44bd3edddd1275c337c3ea1fa92b449ea9cf791f
Natasha Osborne <admin@natashaosborne.com>
2024-07-27 23:02:36 -0600

    extract rotating stargate component change 2d portal to a torus, fix camera and add static image for behind the portal

---
+++ app/components/rotating-portal.js
@@ -0,0 +1,53 @@
+'use client';
+import React, { useRef, useState, useEffect } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useTexture, Torus } from '@react-three/drei';
+import * as THREE from 'three';
+
+export default function RotatingPortal({imageUrl, staticImageUrl, backgroundUrl, width = 3}) {
+  const rotatingMeshRef = useRef();
+  const staticMeshRef = useRef();
+  const backgroundTexture = useTexture(backgroundUrl);
+  const staticTexture = useTexture(staticImageUrl);
+  const portalTexture = useTexture(imageUrl);
+  const [rotation, setRotation] = useState(true);
+
+  //portal texture is warped due to torus shape, so add a repeating pattern for a better result
+  useEffect(() => {
+    portalTexture.repeat.set(60, 1);
+    portalTexture.wrapS = portalTexture.wrapT = THREE.RepeatWrapping;
+    portalTexture.needsUpdate = true;
+  }, [Torus, portalTexture]);
+
+
+  //Rotate the image
+  useFrame(() => {
+    if (rotation && rotatingMeshRef.current) {
+      rotatingMeshRef.current.rotation.z -= 0.01; // Rotate clockwise (negative value for clockwise rotation)
+    }
+  });
+
+  return (
+    <>
+      {/* Background Image */}
+      <mesh position={[0, 0, -10]}>
+        <planeGeometry args={[100, 100]} />
+        <meshStandardMaterial map={backgroundTexture} />
+      </mesh>
+
+      {/* Rotating Torus for Portal */}
+      <mesh ref={rotatingMeshRef} position={[0, 0, 0]}>
+        <Torus args={[width, width / 10, 16, 300]}>
+          <meshStandardMaterial map={portalTexture} />
+        </Torus>
+      </mesh>
+
+      {/* Static Circular Image */}
+      <mesh ref={staticMeshRef} position={[0, 0, -0.1]} rotation={[0, Math.PI, 0]}>
+        <circleGeometry args={[3, 32]} />
+        <meshBasicMaterial map={staticTexture} side={THREE.DoubleSide} />
+      </mesh>
+
+    </>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,96 +1,15 @@
 'use client';
-import React, {useRef, useState, useEffect, useMemo} from 'react';
+import React, {useState, useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 import {useRouter} from 'next/navigation';
-import {Canvas, useFrame} from '@react-three/fiber';
-import {OrbitControls, useTexture, useProgress} from '@react-three/drei';
 
-//TODO: extract
-/* eslint-disable no-unused-vars */
-const usePreloader = () => {
-  /* eslint-enable no-unused-vars */
-  const {progress} = useProgress();
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    if (progress === 100) {
-      setLoaded(true);
-    }
-  }, [progress]);
-
-  return loaded;
-};
-
-//#TODO: extract
-function RotatingImage({imageUrl, staticImageUrl, backgroundUrl, width = 3}) {
-  const rotatingMeshRef = useRef();
-  const staticMeshRef = useRef();
-  const texture = useTexture(imageUrl);
-  const staticTexture = useTexture(staticImageUrl);
-  const backgroundTexture = useTexture(backgroundUrl);
-  const [rotation, setRotation] = useState(true);
-
-  // Load and memoize the sound
-  //#TODO: this is a specialized behavior. I should extract this (pass in soundurl or something)
-  const soundUrl = '/stargate_lock_shevron_sound.mp3';
-  const sound = useMemo(() => new Audio(soundUrl), [soundUrl]);
-
-  // Toggle rotation every 5 seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setRotation(false);
-      setTimeout(() => setRotation(true), 2000); // Pause for 2 seconds
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  // Play the sound when rotation stops
-  useEffect(() => {
-    if (!rotation) {
-      sound.play().catch(() => {
-        // #TODO: I think I should display a sound button, muted when this fails.
-      });
-    }
-  }, [rotation, sound]);
-
-  // Rotate the image
-  useFrame(() => {
-    if (rotation && rotatingMeshRef.current) {
-      rotatingMeshRef.current.rotation.z -= 0.01; // Rotate clockwise (negative value for clockwise rotation)
-    }
-  });
-
-  // Calculate width for static plane
-  const staticPlaneRadius = width / 2.75;
-
-  return (
-    <>
-      {/* Background Image */}
-      <mesh position={[0, 0, -5]}>
-        <planeGeometry args={[20, 20]} />
-        <meshBasicMaterial map={backgroundTexture} />
-      </mesh>
-
-      {/* Rotating Image */}
-      <mesh ref={rotatingMeshRef}>
-        <circleGeometry args={[width / 2, 32]} />
-        <meshBasicMaterial map={texture} />
-      </mesh>
-
-      {/* Static Image */}
-      <mesh ref={staticMeshRef} position={[0, 0, 0.2]}>
-        <circleGeometry args={[staticPlaneRadius, 32]} />
-        <meshBasicMaterial map={staticTexture} />
-      </mesh>
-    </>
-  );
-}
+import RotatingPortal from './components/rotating-portal';
 
 export default function ImageCanvas({
   imageUrl = '/stargate_outer_ring_original.png',
   staticImageUrl = '/initial_inner_wormhole.png',
-  backgroundUrl = '/unknown_space_location.png',
-  width = 3,
+  backgroundUrl = '/unknown_space_location.png',  width = 3,
 }) {
   const router = useRouter();
 
@@ -107,14 +26,15 @@ export default function ImageCanvas({
 
     loadAssets();
   }, []);
+  //ORIGINAL DONT MODIFY
 
-  useEffect(() => {
-    if (loaded) {
-      //TODO: use transition with ship, extract
-      router.push('/home'); // Navigate to the home page
-      return;
-    }
-  }, [loaded, router]);
+  // useEffect(() => {
+  //   if (loaded) {
+  //     //TODO: use transition with ship, extract
+  //     router.push('/home'); // Navigate to the home page
+  //     return;
+  //   }
+  // }, [loaded, router]);
 
   //TODO: animation should go for at least 3 seconds before transitioning
 
@@ -122,10 +42,11 @@ export default function ImageCanvas({
     <div className="max-h-screen flex flex-col items-center justify-center">
       <main className="flex flex-col items-center justify-center w-full">
         <div className="w-full h-[100vh]">
-          <Canvas camera={{position: [0, 0, 5], fov: 75}}>
+          {/* <Canvas camera={{ position: [0, 0, 10], near: 0.1, far: 100 }}> */}
+          <Canvas camera={{position: [0, 0, 10], fov: 75}}>
             <ambientLight />
             <OrbitControls enableZoom={true} enablePan={true} />
-            <RotatingImage
+            <RotatingPortal
               imageUrl={imageUrl}
               staticImageUrl={staticImageUrl}
               backgroundUrl={backgroundUrl}
