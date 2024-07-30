bd026111b649b51249ee4778b7c0ba7df04082aa
Natasha Osborne <admin@natashaosborne.com>
2024-07-30 15:33:02 -0600

    lint

--- app/home/page.js
+++ app/home/page.js
@@ -2,11 +2,17 @@ export default function Home() {
   return (
     <main className="h-full block">
       <div className="pt-40 bg-surface-of-europa bg-cover h-full w-full text-center">
-        <header className="block h-3 text-white text-xl pb-10 font-extralight">NATASHA OSBORNE</header>
-        <p className="block h-3 text-white text-6xl pb-10 font-bold">Software Engineer</p>
+        <header className="block h-3 text-white text-xl pb-10 font-extralight">
+          NATASHA OSBORNE
+        </header>
+        <p className="block h-3 text-white text-6xl pb-10 font-bold">
+          Software Engineer
+        </p>
       </div>
       <div className="pt-40 bg-rocky-mountain-1 bg-cover h-full w-full text-center">
-        <header className="block h-3 text-white text-xl pt-10 font-extralight">BASED IN COLORADO, USA</header>
+        <header className="block h-3 text-white text-xl pt-10 font-extralight">
+          BASED IN COLORADO, USA
+        </header>
       </div>
     </main>
   );
