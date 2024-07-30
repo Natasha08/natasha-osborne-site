3aa2e9125ad0c22d2bdd1df0328e6872e9dbdeb0
Natasha Osborne <admin@natashaosborne.com>
2024-07-29 19:35:26 -0600

    fix navbar for portal scene remove unused code

--- app/components/app-nav.js
+++ app/components/app-nav.js
@@ -13,28 +13,31 @@ const PAGES = [
 /*
   #TODO:
   - mobile navbar
-  - make the background image for the navbar change based on which page you're on
   - make loading page background everywhere black
 */
+
+const setClassForText = (selectedIndex, index) => {
+  if (selectedIndex === index)
+    return 'text-interactive border-b-2 font-medium border-blue-500';
+  return 'text-white';
+};
+
+const navBackgroundClasses = (pathname) => {
+  if (pathname) {
+    return 'bg-earth-sky-view bg-opacity-0';
+  }
+
+  return 'bg-black bg-opacity-50 text-text fixed top-0 left-0 w-full z-10';
+};
 
 export default function AppNav() {
   const [selectedIndex, setSelectedIndex] = useState(null);
   const [navInitialized, setNavInitialized] = useState(false);
   const pathname = usePathname().replace(/\//g, '');
   const navigationLink = '-navigation-link-';
-  // #TODO: make mobile nav
+  // #TODO: for mobile nav
   // const mobileNavigationLink = '-nmobile-avigation-link-';
   var navigationIncrement = 1;
-
-  const saveSelectedindex = (index) => {
-    setSelectedIndex(index);
-  };
-
-  const setClassForText = (selectedIndex, index) => {
-    if (selectedIndex === index)
-      return 'text-interactive border-b-2 font-medium border-blue-500';
-    return 'text-white';
-  };
 
   if (!navInitialized) {
     if (pathname) {
@@ -48,14 +51,14 @@ export default function AppNav() {
 
   return (
     <div className="z-2">
-      <nav className="flex flex-col sm:flex-row-reverse bg-earth-sky-view bg-opacity-0">
+      <nav className={`flex flex-col sm:flex-row-reverse ${navBackgroundClasses(pathname)}`}>
         {PAGES.map((page, index) => (
           <Link
             key={index + navigationLink + navigationIncrement++}
             href={`/${Object.keys(page)[0]}`}
             className={`text-background py-4 px-6 block hover:text-interactive focus:outline-none ${setClassForText(selectedIndex, index)}`}
             aria-current="page"
-            onClick={() => saveSelectedindex(index)}
+            onClick={() => setSelectedIndex(index)}
           >
             {Object.values(page)[0]}
           </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
         'earth-sky-view': 'url(/a-sky-view-of-earth-from-suomi-npp.jpg)',
+        'colorful-stars-and-nebulae': 'url(/colorful_stars_and_nebulae.jpg)'
       },
       screens: {
         'rotate': {'raw': '(orientation: landscape)'},
@@ -22,28 +23,10 @@ module.exports = {
         'background': 'rgb(26, 41, 92)',    // deep space blue
         'interactive': 'rgb(173, 121, 95)', // nebula orange
         'text': '#F8F8F8',                  // starlight white
-
-        // 'background': '#0B3D91',  // deep space blue
-
-        // 'interactive': '#F5A623',// nebula orange
         'important': '#8A2BE2',  // nebula purple
-        'highlight': '#FFD700'   // star yellow
-
-
-        // 'primary': 'var(--primary-color)',
-        // 'secondary': 'var(--secondary-color)',
-        // 'text-color': '#1D3557',
-        // 'background': 'var(--background-color)',
-        // 'accent': 'var(--accent-color)'
-
-
-        // space: '#0B0C10',
-        // darkBlue: '#1F2833',
-        // lightBlue: '#66FCF1',
-        // neonBlue: '#45A29E',
-        // charcoal: '#C5C6C7',
-        // white: '#F8F8F8',
-        // lightGray: '#F1FAEE'
+        'highlight': '#FFD700',   // star yellow
+        'spam': 'rgba(255,255,255,0.3)',
+        'ham': 'rgba(255,255,255,0.3)'
       },
     },
   },
