687a1ed70bf964f58f071ff25b9e1f2a30dff217
Natasha Osborne <admin@natashaosborne.com>
2024-07-29 22:35:50 -0600

    add mobile menu close menu when navigating to new page styling

--- app/components/app-nav.js
+++ app/components/app-nav.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import Link from 'next/link';
 import {usePathname} from 'next/navigation';
 
@@ -10,26 +10,34 @@ const PAGES = [
   {home: 'Home'},
 ];
 
-/*
-  #TODO:
-  - mobile navbar
-  - need a better loading page
-*/
+const PagesList = ({selectedIndex, setSelectedIndex, navigationLink, isMobile = false}) => (
+  PAGES.map((page, index) => (
+    <Link
+      key={`${index}-${navigationLink}`}
+      href={`/${Object.keys(page)[0]}`}
+      className={`text-background py-4 px-6 block hover:text-interactive focus:outline-none ${setClassForText(selectedIndex, index, isMobile)}`}
+      aria-current="page"
+      onClick={() => setSelectedIndex(index)}
+    >
+      {Object.values(page)[0]}
+    </Link>
+  ))
+);
 
-const setClassForText = (selectedIndex, index) => {
+const setClassForText = (selectedIndex, index, isMobile) => {
   if (selectedIndex === index)
-    return 'text-interactive border-b-2 font-medium border-blue-500';
+    return `text-interactive font-medium ${!isMobile ? 'border-blue-500 border-b-2' : ''}`;
   return 'text-white';
 };
 
 export default function AppNav() {
   const [selectedIndex, setSelectedIndex] = useState(null);
   const [navInitialized, setNavInitialized] = useState(false);
-  const pathname = usePathname().replace(/\//g, '');
+  const [toggleMobileMenu, setToggleMobileMenu] = useState(false);
+  const parsedPathname = usePathname().replace(/\//g, '');
+  const [pathname, setPathname] = useState(parsedPathname);
   const navigationLink = '-navigation-link-';
-  // #TODO: for mobile nav
-  // const mobileNavigationLink = '-nmobile-avigation-link-';
-  var navigationIncrement = 1;
+  const mobileNavigationLink = '-nmobile-avigation-link-';
 
   if (!navInitialized) {
     if (pathname) {
@@ -41,21 +49,59 @@ export default function AppNav() {
     }
   }
 
-  return (
-    <div className="z-2">
-      <nav className="flex flex-col sm:flex-row-reverse bg-black bg-opacity-50 text-text fixed top-0 left-0 w-full z-10">
-        {PAGES.map((page, index) => (
-          <Link
-            key={index + navigationLink + navigationIncrement++}
-            href={`/${Object.keys(page)[0]}`}
-            className={`text-background py-4 px-6 block hover:text-interactive focus:outline-none ${setClassForText(selectedIndex, index)}`}
-            aria-current="page"
-            onClick={() => setSelectedIndex(index)}
-          >
-            {Object.values(page)[0]}
-          </Link>
-        ))}
+  useEffect(() => {
+    if (parsedPathname != pathname) {
+      setPathname(parsedPathname);
+      setToggleMobileMenu(false);
+    }
+
+  }, [pathname, parsedPathname])
+
+  const NavigationList = ({containerClasses, navClasses}) => (
+    <div className={containerClasses}>
+      <nav className={navClasses}>
+        <PagesList
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+          navigationLink={navigationLink}
+        />
       </nav>
     </div>
+  );
+
+  const MobileNavigationList = ({containerClasses, navClasses, navId}) => (
+    <div className={containerClasses}>
+      <button className="fixed text-blue-600 p-3" aria-controls={navId} onClick={() => setToggleMobileMenu(!toggleMobileMenu)}>
+        <svg className="block h-4 w-4 fill-current text-white" viewBox="0 0 20 20">
+          <title>Mobile menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+        </svg>
+      </button>
+      <div className={navClasses} id={navId}>
+        <div className="fixed top-10">
+          <PagesList
+            selectedIndex={selectedIndex}
+            setSelectedIndex={setSelectedIndex}
+            mobileNavigationLink={mobileNavigationLink}
+            isMobile={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <>
+      <NavigationList
+        containerClasses="hidden md:block z-10"
+        navClasses="flex flex-col sm:flex-row-reverse bg-black bg-opacity-50 text-text fixed top-0 left-0 w-full "
+        ariaControls="mobile-menu"
+      />
+      <MobileNavigationList
+        containerClasses="shown md:hidden z-10"
+        navClasses={`flex flex-col sm:flex-row-reverse bg-black bg-opacity-50 fixed text-text top-0 right-0 w-full ${toggleMobileMenu ? 'shown' : 'hidden'}`}
+        navId="mobile-menu"
+      />
+    </>
   );
 }
