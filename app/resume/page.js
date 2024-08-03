b3442c22b16cf85254adca4c704256113ba316b4
Natasha Osborne <admin@natashaosborne.com>
2024-08-03 05:15:44 -0600

    lint

--- app/resume/page.js
+++ app/resume/page.js
@@ -6,10 +6,15 @@ export default async function Resume() {
   return (
     <main className="h-full">
       {resumeUrl ? (
-        <iframe className="h-full w-full pt-20" src={resumeUrl} />
+        <iframe
+          title="Natasha Osborne's Resume"
+          alt="Natasha Osborne's Resume"
+          className="h-full w-full pt-20"
+          src={resumeUrl}
+        />
       ) : (
         <p>Loading image...</p>
       )}
     </main>
   );
-};
+}
