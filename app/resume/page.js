4acda4244cf41a2c9240a8ba625a08d38b8d5ccd
Natasha Osborne <admin@natashaosborne.com>
2024-08-03 05:14:26 -0600

    create .env example file create api method to get signed url from aws

--- app/resume/page.js
+++ app/resume/page.js
@@ -1,3 +1,15 @@
-export default function Resume() {
-  return <main className="bg-earth-sky-view bg-cover h-full"></main>;
-}
+import {getS3SignedUrl} from '@/lib/api';
+
+export default async function Resume() {
+  const resumeUrl = await getS3SignedUrl(process.env.RESUME_FILE_NAME);
+
+  return (
+    <main className="h-full">
+      {resumeUrl ? (
+        <iframe className="h-full w-full pt-20" src={resumeUrl} />
+      ) : (
+        <p>Loading image...</p>
+      )}
+    </main>
+  );
+};
