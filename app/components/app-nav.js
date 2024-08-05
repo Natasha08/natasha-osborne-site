0f005928ab03c6d85fd22c568ea6851ed256859b
Natasha Osborne <admin@natashaosborne.com>
2024-08-04 19:22:16 -0600

    lint

--- app/components/app-nav.js
+++ app/components/app-nav.js
@@ -1,6 +1,5 @@
 'use client';
 
-import {useEffect, useState} from 'react';
 import {
   HomeIcon,
   UserIcon,
@@ -34,7 +33,11 @@ const Menu = ({activeSection, sectionRefs, pages, isMobile = false}) => (
                 className={`size-6 hover:text-interactive ${setClassForText(activeSection, page.id, isMobile)}`}
               />
             </button>
-            <span id={`tooltip-default-${index}`} role="tooltip" className="absolute z-10 inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-deep-blue rounded-lg shadow-sm opacity-0 tooltip z-20 hover:visible group-hover:opacity-100">
+            <span
+              id={`tooltip-default-${index}`}
+              role="tooltip"
+              className="absolute z-10 inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-deep-blue rounded-lg shadow-sm opacity-0 tooltip z-20 hover:visible group-hover:opacity-100"
+            >
               {page.label}
             </span>
           </li>
