1d6e5ab8954f950f514d1dbf6d77d66a11790e71
Natasha Osborne <admin@natashaosborne.com>
2024-08-18 03:34:26 -0600

    more home page styling fix an issue where w-screen stopped the menu from functioning when it was on the home component

--- app/components/home.js
+++ app/components/home.js
@@ -1,17 +1,18 @@
+import Image from 'next/image';
 import StealthItem from '@/components/stealth-item';
 import characterImage from '@/public/character_leaning.png';
+import characterAtDesk from '@/public/character_at_desk.jpeg';
 
 const Home = () => (
-  <div className="pb-12">
+  <div className="pb-12 h-screen flex flex-col justify-center">
     <div
-      className="fixed inset-0 bg-cover bg-center z-0 w-screen home h-screen"
+      className="fixed inset-0 bg-cover bg-center z-0 w-screen h-screen"
       style={{
-        background: `radial-gradient(circle at center, rgb(26, 41, 92) 0%, rgb(42, 63, 136) 60%, rgb(19, 30, 67) 100%)`,
+        background: `radial-gradient(circle at top, rgb(26, 41, 92) 0%, rgb(34, 51, 100) 50%, rgb(19, 30, 67) 100%)`,
       }}
-    >
-    </div>
-    <div className="pt-28 flex flex-col h-screen w-screen items-center mx-auto">
-      <header className="text-center text-4xl pb-10 font-bold z-10 tracking-wider">
+    ></div>
+    <div className="pt-10 flex flex-col items-center justify-center mx-auto z-10">
+      <header className="text-center text-5xl pb-8 font-extrabold tracking-widest text-white">
         NATASHA OSBORNE
         <StealthItem
           src={characterImage}
@@ -20,13 +21,24 @@ const Home = () => (
           width="12"
         />
       </header>
-      <p className="text-center text-3xl font-semibold pb-6 hover:text-blue-500 transition-colors duration-300 text-muted-gold">
+      <p className="text-center text-4xl font-semibold pb-4 hover:text-blue-500 transition-colors duration-300 text-muted-gold">
         Full Stack Software Engineer
       </p>
-      <p className="text-center text-xl font-light pt-4">
+      <p className="text-center text-2xl font-light pt-2 text-white">
         BASED IN COLORADO, USA
       </p>
-      <img src="/character_at_desk.jpeg" alt="drawing of developer at computer desk" className="character mt-10 h-auto w-1/2 relative rounded-lg shadow-2xl shadow-interactive" />
+      <div className="mt-8 h-auto flex justify-center">
+        <div className="relative max-w-md rounded-lg shadow-2xl shadow-interactive">
+          <Image
+            src={characterAtDesk}
+            alt="Character at desk"
+            width={500}
+            height={300}
+            className="rounded-lg"
+            priority
+          />
+        </div>
+      </div>
     </div>
   </div>
 );
