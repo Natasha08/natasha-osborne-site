aeb7ea794be98676479f1358469ee6daa9e8f9b5
Natasha Osborne <admin@natashaosborne.com>
2024-10-06 12:26:43 -0600

    update project data

--- app/projects/page.js
+++ app/projects/page.js
@@ -3,115 +3,7 @@
 import {useState} from 'react';
 import MouseGlow from '@/components/mouse-glow';
 import SkillBubble from '@/components/skill-bubble';
-
-const projectsData = [
-  {
-    title: 'Workout App',
-    year: 2016,
-    technologies: ['PHP'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App 2',
-    year: 2016,
-    technologies: ['Node'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App 3',
-    year: 2017,
-    technologies: ['Angular', 'Node'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App',
-    year: 2016,
-    technologies: ['PHP'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App 2',
-    year: 2016,
-    technologies: ['Node', 'AWS', 'Spam'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App 3',
-    year: 2017,
-    technologies: ['Angular', 'Node'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App',
-    year: 2016,
-    technologies: ['PHP', 'React', 'TailwindCSS'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App 2',
-    year: 2016,
-    technologies: ['Node'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App 3',
-    year: 2017,
-    technologies: ['Angular', 'Node'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App',
-    year: 2016,
-    technologies: ['PHP'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App 2',
-    year: 2016,
-    technologies: ['Node'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App 3',
-    year: 2017,
-    technologies: ['Angular', 'Node'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App',
-    year: 2016,
-    technologies: ['PHP'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App 2',
-    year: 2016,
-    technologies: ['Node'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  {
-    title: 'Workout App 3',
-    year: 2017,
-    technologies: ['Angular', 'Node'],
-    madeAt: 'Personal Project',
-    link: '#',
-  },
-  // Add more project data as needed
-];
+import projectsData from '@/lib/data/projects.json';
 
 const DisplaySkillBubbles = ({skills = []}) => {
   return (
@@ -175,25 +67,27 @@ const ProjectsPage = () => {
                   className="border-2 border-x-0 border-gray-500 hover:bg-gray-500 hover:border-dotted"
                 >
                   <td className="px-4 py-2">{project.title}</td>
-                  <td className="px-4 py-2 hidden sm:block">{project.year}</td>
-                  <td className="px-4 py-2 list-none opacity-0 sm:">
+                  <td className="px-4 py-4 hidden sm:block">{project.year}</td>
+                  <td className="px-4 py-2 list-none">
                     <DisplaySkillBubbles skills={project.technologies} />
                   </td>
-                  <td className="px-4 py-2 hidden md:block">
+                  <td className="px-4 py-4 hidden md:block">
                     {project.madeAt}
                   </td>
                   <td className="px-4 py-2 text-blue-600 hover:text-blue-800">
-                    <a href={project.link} className="flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        className="w-4 h-4 mr-2"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.22 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.19 0 .21.15.46.55.38C13.71 14.53 16 11.54 16 8c0-4.42-3.58-8-8-8z" />
-                      </svg>
-                      Github
-                    </a>
+                    {project.link ? (
+                      <a href={project.link} className="flex items-center">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          className="w-4 h-4 mr-2"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.22 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.19 0 .21.15.46.55.38C13.71 14.53 16 11.54 16 8c0-4.42-3.58-8-8-8z" />
+                        </svg>
+                        Github
+                      </a>
+                    ) : null}
                   </td>
                 </tr>
               ))
